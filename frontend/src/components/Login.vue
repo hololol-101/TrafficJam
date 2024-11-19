40990fae402fe90444a37dc4ddcd74e3fff7d93f
노강은 <riveris@nogang-eun-ui-MacBookAir.local>
2024-11-19 21:05:44 +0900

    20241119 노강은 fe

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-container>
+    <v-row
+      justify="space-between"
+    >
+      <v-col
+        cols="12"
+        md="4"
+      >
+        <v-form ref="form">
+          <v-text-field
+            v-model="model"
+            :counter="max"
+            :rules="rules"
+            label="ID"
+          ></v-text-field>
+        </v-form>
+      </v-col>
+
+      <v-col
+        cols="12"
+        md="6"
+      >
+        <v-slider
+          v-model="max"
+          label="Max characters"
+        >
+        </v-slider>
+
+        <v-checkbox
+          v-model="allowSpaces"
+          label="Allow spaces"
+        ></v-checkbox>
+
+        <v-text-field
+          v-model="match"
+          label="Value must match"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<template>
+  <v-container>
+    <v-row
+
+      justify="space-between"
+    >
+      <v-col
+        cols="12"
+        md="4"
+      >
+        <v-form ref="form">
+          <v-text-field
+            v-model="model"
+            :counter="max"
+            :rules="rules"
+            label="Password"
+          ></v-text-field>
+        </v-form>
+      </v-col>
+
+      <v-col
+        cols="12"
+        md="6"
+      >
+        <v-slider
+          v-model="max"
+          label="Max characters"
+        >
+        </v-slider>
+
+        <v-checkbox
+          v-model="allowSpaces"
+          label="Allow spaces"
+        ></v-checkbox>
+
+        <v-text-field
+          v-model="match"
+          label="Value must match"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
