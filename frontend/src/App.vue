dcf6179475ee9eecb18cf1949fe324780026faa8
노강은 <riveris@nogang-eun-ui-MacBookAir.local>
2024-12-23 02:58:08 +0900

    20241223 노강은 Navigation 개발

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,50 @@
 <template>
   <div id="app">
-    <router-link to="/main">main</router-link>
-    <router-link to="/login">login</router-link>
-    <!-- convert -->
-    <!-- <a href="#/main" class="">main</a> -->
-    <router-view></router-view>
+    <v-row>
+        <v-navigation-drawer permanent>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="text-h6">
+                Page
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider></v-divider>
+
+          <v-list
+              dense
+              nav
+          >
+            <v-list-item
+                v-for="item in items"
+                :key="item.title"
+                :to="item.link"
+                link
+            >
+              <v-list-item-content>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+      <v-col>
+        <router-view></router-view>
+      </v-col>
+    </v-row>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        { title: 'Main', link: '/' },
+        { title: 'Login', link: '/login' },
+        { title: 'Order', link: '/order' },
+        { title: 'Pay', link: '/pay' },
+      ],
+      right: null,
+    }
+  },
+}</script>
