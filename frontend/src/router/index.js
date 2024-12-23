491360bc4d757a0a37c7d9a3d5d7ba5b334c1b1c
노강은 <riveris@nogang-eun-ui-MacBookAir.local>
2024-12-24 00:17:13 +0900

    20241224 노강은 Vue Router 모드 Hash로 수정 Fix #7

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,18 +2,28 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import HelloWorld from "@/components/HelloWorld.vue";
 import LoginPage from "@/components/LoginPage.vue";
+import LoadingPage from "@/components/LoadingPage.vue";
+import OrderPage from "@/components/OrderPage.vue";
 Vue.use(VueRouter);
 
 export default new VueRouter({
-    mode:'history', //해쉬값 제거 방식
+    mode:'hash', //해쉬값 제거 방식
     routes: [{
         path: '/',
         redirect: '/home'
     }, {
         path: '/home',
+        name: 'home',
         component: HelloWorld,
     }, {
         path: '/login',
+        name: 'login',
         component: LoginPage
+    },{
+        path: '/loading',
+        component: LoadingPage
+    }, {
+        path: '/order',
+        component: OrderPage
     }]
 });
