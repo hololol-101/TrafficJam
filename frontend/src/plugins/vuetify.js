4397496c79542cf69003760736842a9147478e5f
노강은 <riveris@nogang-eun-ui-MacBookAir.local>
2024-12-27 04:04:30 +0900

    20241227 노강은 기본 화면 색상 변경

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -4,4 +4,13 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: '#FFA000',
+                secondary: '#FFECB3',
+                accent: '#FFD740',
+            },
+        },
+    },
 });
